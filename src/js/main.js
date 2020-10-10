2e42a38bf2a35160e86659e9b34bb2172254ca64
Eryk Slocinski <eryk.slocinski@gmail.com>
2020-10-10 21:17:39 +0200

    Added ligtbox view with list and custom scroll for mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,27 @@
-import './hamburger-menu';
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.navigation');
+const body = document.querySelector('body');
+const close = document.querySelector('.lightbox__close');
+const lightbox = document.querySelector('.lightbox');
+const heroButton = document.querySelector('.hero__button');
+
+const handleHamburgerClick = () => {
+  hamburger.classList.toggle('hamburger--active');
+  hamburger.setAttribute('aria-expanded', hamburger.classList.contains('hamburger--active'));
+  nav.classList.toggle('navigation--active');
+  body.classList.toggle('hidden');
+};
+
+const handleLightboxClose = () => {
+  body.classList.remove('hidden');
+  lightbox.classList.remove('lightbox--active');
+};
+
+const handleLightboxOpen = () => {
+  body.classList.add('hidden');
+  lightbox.classList.add('lightbox--active');
+};
+
+hamburger.addEventListener('click', handleHamburgerClick);
+close.addEventListener('click', handleLightboxClose);
+heroButton.addEventListener('click', handleLightboxOpen);
