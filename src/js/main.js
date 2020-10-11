1250b85ab1234258e225871550d9ef801db2a0b1
Eryk Slocinski <eryk.slocinski@gmail.com>
2020-10-11 16:20:14 +0200

    Added hover animations for buttons and anchors, lightbox desktop view finished

--- src/js/main.js
+++ src/js/main.js
@@ -4,22 +4,26 @@ const body = document.querySelector('body');
 const close = document.querySelector('.lightbox__close');
 const lightbox = document.querySelector('.lightbox');
 const heroButton = document.querySelector('.hero__button');
+const overlay = document.querySelector('.overlay');
 
 const handleHamburgerClick = () => {
   hamburger.classList.toggle('hamburger--active');
   hamburger.setAttribute('aria-expanded', hamburger.classList.contains('hamburger--active'));
   nav.classList.toggle('navigation--active');
   body.classList.toggle('hidden');
+  overlay.classList.toggle('overlay--active');
 };
 
 const handleLightboxClose = () => {
   body.classList.remove('hidden');
   lightbox.classList.remove('lightbox--active');
+  overlay.classList.toggle('overlay--active');
 };
 
 const handleLightboxOpen = () => {
   body.classList.add('hidden');
   lightbox.classList.add('lightbox--active');
+  overlay.classList.toggle('overlay--active');
 };
 
 hamburger.addEventListener('click', handleHamburgerClick);
